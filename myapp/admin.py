9b9e2bcd2adaa9b76275ca5fd59b332cf554fff1
Vladislav Sterin <sterinbiz@gmail.com>
2023-02-25 20:47:00 +0200

    all models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
-from myapp.models import UserProfile
+from myapp.models import UserProfile, Post, Tag, Photo
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
 
 class UserProfileInLine(admin.StackedInline):
     model = UserProfile
@@ -19,3 +19,6 @@ class AccountsUserAdmin(AuthUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, AccountsUserAdmin)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Photo)
--- myapp/models.py
+++ myapp/models.py
@@ -18,9 +18,25 @@ def create_use_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 
-class VisualContent():
-    image = models.ImageField(upload_to='icons/% Y/% m/% d/')
+class Tag(models.Model):
+    name = models.CharField(max_length=120, null=True)
+    def __str__(self):
+        return self.name
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=150, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    likes = models.ManyToManyField(UserProfile)
+    def __str__(self):
+        return self.title
 
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.pk
 
