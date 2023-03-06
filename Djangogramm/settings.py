4e33b39c1f96009135783b52bd25eca0d1251961
Vladislav Sterin <sterinbiz@gmail.com>
2023-03-06 15:32:18 +0200

    added static files v2

--- Djangogramm/settings.py
+++ Djangogramm/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
